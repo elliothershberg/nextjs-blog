1733b0709011db54e03dd9cde45e2e36b3bc8562
Elliot Hershberg <eahershberg@gmail.com>
2022-08-17 14:51:41 -0700

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Yo! I'm Elliot 🧬</p>
+        <p>Vercel is incredible...</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
